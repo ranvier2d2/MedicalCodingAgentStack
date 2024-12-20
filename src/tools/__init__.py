eca92b5478f8f53da1e6326d62f7cae754e62e04
ranvier2d2 <r2d2@ranvier-technologies.com>
2024-12-19 23:36:54 -0300

    AgentStack working with custom tools and a gpt-4 instance as a tool

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,5 +1,7 @@
 from .file_read_tool import file_read_tool
 from .custom_tool import MyCustomTool
+from .gpt4_suggestion_tool import Gpt4SuggestionTool
 
 custom_tool = MyCustomTool()
+gpt4_suggestion_tool = Gpt4SuggestionTool()
 # tool import
--- src/tools/gpt4_suggestion_tool.py
+++ src/tools/gpt4_suggestion_tool.py
@@ -1,4 +1,4 @@
-from crewai.tools import BaseTool
+from crewai_tools import BaseTool
 from typing import Type
 from pydantic import BaseModel, Field
 import os
